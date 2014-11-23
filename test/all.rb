699266b4b81223c2eff6e6bf15935b45690ee21f
Zee Spencer <zee@zeespencer.com>
2014-11-22 21:17:43 -0800

    REFACTOR - Extract tests for con 0,0 to their own file
    I also took the opportunity to extract a shared test type so I can start
    testing cons at a different location.

--- test/all.rb
+++ test/all.rb
@@ -1,14 +1,10 @@
 require 'minitest/autorun'
+
 path_to_lib = File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 $LOAD_PATH.unshift(path_to_lib)
-
 require 'game_of_life'
 
-class TestAConAtZeroZeroAtEndOfDay < Minitest::Test
-  def target_con
-    { :x => 0, :y => 0 }
-  end
-
+class ConTest < Minitest::Test
   def next_generation(cons)
     GameOfLife.next_generation(cons)
   end
@@ -20,64 +16,10 @@ class TestAConAtZeroZeroAtEndOfDay < Minitest::Test
   def refute_alive(con, population)
     refute population.include?(con), "Con #{target_con} was supposed to be dead but was found in #{population}"
   end
-
-  def test_dies_when_standing_alone
-    cons = [ target_con ]
-    refute_alive(target_con, next_generation(cons))
-  end
-
-  def test_stays_alive_with_west_and_east_neighbors
-    cons = [ target_con,
-             { :x => 1,  :y => 0 },
-             { :x => -1, :y => 0 } ]
-
-    assert_alive(target_con, next_generation(cons))
-  end
-
-  def test_dies_with_four_neighbors
-    cons = [ target_con,
-             { :x => 1,  :y => -1 },
-             { :x => 1,  :y => 0  },
-             { :x => -1, :y => 0  },
-             { :x => -1, :y => -1 } ]
-
-    refute_alive(target_con, next_generation(cons))
-  end
-
-  def test_stays_alive_with_east_and_west_neighbors_despite_other_cons_in_population
-    cons = [ target_con,
-             { :x => 1,  :y => -1 },
-             { :x => 1,  :y => 0  },
-             { :x => -3, :y => 3  },
-             { :x => -3, :y => -3 } ]
-
-    assert_alive(target_con, next_generation(cons))
-  end
-
-  def test_stays_alive_with_north_and_south_neighbors_despite_other_cons_in_population
-    cons = [ target_con,
-             { :x => 0,  :y => -1 },
-             { :x => 0,  :y => 1  },
-             { :x => -3, :y => 3  },
-             { :x => -3, :y => -3 } ]
-
-    assert_alive(target_con, next_generation(cons))
-  end
-
-  def test_dies_with_no_neighors_even_if_non_neighbors_are_east
-    cons = [ target_con,
-             { :x => -5,  :y => -1 },
-             { :x => -5,  :y => 0  },
-             { :x => -5,  :y => 1  }]
-
-    refute_alive(target_con, next_generation(cons))
-  end
-
-  def test_dies_even_if_there_are_three_of_the_same_cell
-    cons = [ target_con,
-             target_con,
-             target_con]
-
-    refute_alive(target_con, next_generation(cons))
-  end
 end
+
+
+path_to_test = File.expand_path(File.join(File.dirname(__FILE__)))
+$LOAD_PATH.unshift(path_to_test)
+
+require 'test_a_con_at_0_0_at_end_of_day'
---
+++ test/test_a_con_at_0_0_at_end_of_day.rb
@@ -0,0 +1,65 @@
+class TestAConAtZeroZeroAtEndOfDay < ConTest
+  def target_con
+    { :x => 0, :y => 0 }
+  end
+
+  def test_dies_when_standing_alone
+    cons = [ target_con ]
+    refute_alive(target_con, next_generation(cons))
+  end
+
+  def test_stays_alive_with_west_and_east_neighbors
+    cons = [ target_con,
+             { :x => 1,  :y => 0 },
+             { :x => -1, :y => 0 } ]
+
+    assert_alive(target_con, next_generation(cons))
+  end
+
+  def test_dies_with_four_neighbors
+    cons = [ target_con,
+             { :x => 1,  :y => -1 },
+             { :x => 1,  :y => 0  },
+             { :x => -1, :y => 0  },
+             { :x => -1, :y => -1 } ]
+
+    refute_alive(target_con, next_generation(cons))
+  end
+
+  def test_stays_alive_with_east_and_west_neighbors_despite_other_cons_in_population
+    cons = [ target_con,
+             { :x => 1,  :y => -1 },
+             { :x => 1,  :y => 0  },
+             { :x => -3, :y => 3  },
+             { :x => -3, :y => -3 } ]
+
+    assert_alive(target_con, next_generation(cons))
+  end
+
+  def test_stays_alive_with_north_and_south_neighbors_despite_other_cons_in_population
+    cons = [ target_con,
+             { :x => 0,  :y => -1 },
+             { :x => 0,  :y => 1  },
+             { :x => -3, :y => 3  },
+             { :x => -3, :y => -3 } ]
+
+    assert_alive(target_con, next_generation(cons))
+  end
+
+  def test_dies_with_no_neighors_even_if_non_neighbors_are_east
+    cons = [ target_con,
+             { :x => -5,  :y => -1 },
+             { :x => -5,  :y => 0  },
+             { :x => -5,  :y => 1  }]
+
+    refute_alive(target_con, next_generation(cons))
+  end
+
+  def test_dies_even_if_there_are_three_of_the_same_cell
+    cons = [ target_con,
+             target_con,
+             target_con ]
+
+    refute_alive(target_con, next_generation(cons))
+  end
+end
