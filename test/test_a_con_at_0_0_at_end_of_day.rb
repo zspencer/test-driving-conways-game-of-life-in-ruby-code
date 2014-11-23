935e0d601393c1b65460a3840e560020243c79dc
Zee Spencer <zee@zeespencer.com>
2014-11-22 22:17:36 -0800

    RED - Bearing a new con doesn't create one for each neighbor

--- test/test_a_con_at_0_0_at_end_of_day.rb
+++ test/test_a_con_at_0_0_at_end_of_day.rb
@@ -70,4 +70,17 @@ class TestAConAtZeroZeroAtEndOfDay < ConTest
 
     assert_alive(target_con, next_generation(cons))
   end
+
+  def test_only_one_is_born_if_there_are_three_neighbors
+    cons = [ { :x => 1, :y => 0 },
+             { :x => -1, :y => 0 },
+             { :x => -1, :y => 1 } ]
+
+    target_cons = next_generation(cons).select do |con|
+      con == target_con
+    end
+
+    assert_equal 1, target_cons.length
+  end
+
 end
