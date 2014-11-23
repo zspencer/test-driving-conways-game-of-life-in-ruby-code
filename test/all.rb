ddf965fe0813fb83b26e49edd3b4ffb0e9513c93
Zee Spencer <zee@zeespencer.com>
2014-11-22 18:36:03 -0800

    RED - Test a con standing alone dies at the end of the day
    I've chosen to start testing at the end-of-day transition. I'm hoping it
    will provide a reasonable entrance point that allows quite a bit of
    interesting code before needing to drop down (or up) into another layer
    of abstraction.
    
    This test demonstrates the pattern of Arrange, Act, Assert:
    
    I "Arrange" by creating a list of the cons alive at this point in time:
    
        cons = [ { :x => 0, :y => 0 } ]
    
    I "Act" by calling the method under test
    (`GameOfLife.transition_at_end_of_day`) with the list of `cons`:
    
        next_days_cons = GameOfLife.transition_at_end_of_day(cons)
    
    I "Assert" by comparing the result of calling the method under test with
    my expected result, in this case an empty array:
    
        assert_equal [], next_days_cons

---
+++ test/all.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+
+class TestTransitionAtEndOfDay < Minitest::Test
+  def test_a_con_standing_alone_dies_at_end_of_day
+    cons = [ { :x => 0, :y => 0 } ]
+
+    next_days_cons = GameOfLife.transition_at_end_of_day(cons)
+
+    assert_equal [], next_days_cons
+  end
+end
