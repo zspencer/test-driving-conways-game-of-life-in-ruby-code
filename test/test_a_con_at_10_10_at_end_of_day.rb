b34036ac957cd7410ff2756c8bdb92332b5c5ec1
Zee Spencer <zee@zeespencer.com>
2014-11-22 22:02:24 -0800

    RED - Test Cell 10,10 is born when it has three neighbors

--- test/test_a_con_at_10_10_at_end_of_day.rb
+++ test/test_a_con_at_10_10_at_end_of_day.rb
@@ -10,4 +10,12 @@ class TestAConAtTenTenAtEndOfDay < ConTest
 
     assert_alive(target_con, next_generation(cons))
   end
+
+  def test_is_born_if_there_are_three_neighbors
+    cons = [ { :x => 10, :y => 11 },
+             { :x => 11,  :y => 11 },
+             { :x => 11, :y => 10 } ]
+
+    assert_alive(target_con, next_generation(cons))
+  end
 end
