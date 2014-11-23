cfce521a4ea67e432aa32daa71d5b8f913d7c88d
Zee Spencer <zee@zeespencer.com>
2014-11-22 21:30:54 -0800

    GREEN - A cell at 10,10 follows all the rules

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -2,23 +2,24 @@ module GameOfLife
   NEIGHBORS_NEEDED_TO_SURVIVE = (2..3)
 
   def self.next_generation(cons)
-    if NEIGHBORS_NEEDED_TO_SURVIVE.cover?(count_neighbors(cons))
-      cons
-    else
-      []
+    cons.select do |con|
+      neighbors = count_neighbors(con, cons)
+      NEIGHBORS_NEEDED_TO_SURVIVE.cover?(neighbors)
     end
   end
 
-  def self.count_neighbors(population)
-    population.select { |con|
-      neighbors?(con)
+  def self.count_neighbors(con, population)
+    population.select { |con_b|
+      neighbors?(con, con_b)
     }.length
   end
 
-  def self.neighbors?(con)
-    con != { :x => 0, :y => 0 } &&
-    con[:x] <= 1 && con[:x] >= -1 &&
-    con[:y] <= 1 && con[:y] >= -1
+  def self.neighbors?(con_a, con_b)
+    con_a != con_b &&
+    con_a[:x] <= con_b[:x] + 1 &&
+    con_a[:x] >= con_b[:x] -1 &&
+    con_a[:y] <= con_b[:x] + 1 &&
+    con_a[:y] >= con_b[:x] -1
   end
 end
 
