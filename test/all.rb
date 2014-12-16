8f2b276a655824470b5c6eca8ef6a698ae9ee376
Zee Spencer <zee@zeespencer.com>
2014-12-16 07:51:32 -0800

    REFACTOR - Extract helper to set up and run the game of life
    Since we had a bit of duplication in both tests, I figured I'd extract a
    helper to make the ARRANGE/ACT steps a bit easier to write.

--- test/all.rb
+++ test/all.rb
@@ -19,16 +19,21 @@ class TurnTaker
 end
 
 class TestGameOfLife < MiniTest::Test
-  def test_an_empty_world_after_a_single_iteration
-    game_of_life = GameOfLife.new([], TurnTaker.new(1))
+  def setup_and_run_game_of_life(world)
+    game_of_life = GameOfLife.new(world, TurnTaker.new(1))
     game_of_life.run!()
+    game_of_life
+  end
+
+  def test_an_empty_world_after_a_single_iteration
+    game_of_life = setup_and_run_game_of_life([])
     assert_equal([], game_of_life.world())
   end
 
   def test_a_world_with_a_con_at_0_0_with_two_neighbors_keeps_the_con_with_the_neighbors_after_a_single_iteration
     starting_world = [{:x => 0, :y => 0}, {:x => 1, :y => 1}, {:x => 0, :y => 1}]
-    game_of_life = GameOfLife.new(starting_world, TurnTaker.new(1))
-    game_of_life.run!()
+
+    game_of_life = setup_and_run_game_of_life(starting_world)
 
     assert(game_of_life.world.include?({:x => 0, :y => 0}), "The Con at 0,0 didn't survive!")
   end
