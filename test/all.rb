a491a0766d0c267dc932a9a229d5a42cc0ff52a2
Zee Spencer <zee@zeespencer.com>
2014-12-16 07:46:27 -0800

    REFACTOR - assert_equal takes expected as first argument
    I always mess this up.

--- test/all.rb
+++ test/all.rb
@@ -22,7 +22,7 @@ class TestGameOfLife < MiniTest::Test
   def test_an_empty_world_after_a_single_iteration
     game_of_life = GameOfLife.new([], TurnTaker.new(1))
     game_of_life.run!()
-    assert_equal(game_of_life.world, [])
+    assert_equal([], game_of_life.world())
   end
 
   def test_a_world_with_a_con_at_0_0_with_two_neighbors_keeps_the_con_with_the_neighbors_after_a_single_iteration
